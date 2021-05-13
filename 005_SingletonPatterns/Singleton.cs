846305980e18284e06057b9a7399272a62b732e1
gitName <a@b.c>
2021-05-13 11:48:13 +0530

    Fixed naming convention.

--- 005_SingletonPatterns/Singleton.cs
+++ 005_SingletonPatterns/Singleton.cs
@@ -11,7 +11,7 @@
             empty = true;
             boiled = false;
         }
-
+            
         //public static ChocolateBoiler GetChocolateBoiler()
         //{
         //    if (chocolateBoiler == null)
